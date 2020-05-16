898947c5141bfb4a4e88fb94eb72cd30a8add79c
Paul Pan <panjunhong05@gmail.com>
2020-05-15 21:03:17 -0700

    Initial project layout

---
+++ src/two_sum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target) {
+  unordered_map<int, vector<int>> unique_nums;
+
+  for (int i = 0; i < nums.size(); ++i) {
+    if (unique_nums.count(nums[i]) > 0) {
+      unique_nums[nums[i]].push_back(i);
+    } else {
+      unique_nums.insert(make_pair(nums[i], vector<int>()));
+      unique_nums[nums[i]].push_back(i);
+    }
+  }
+
+  for (int i = 0; i < nums.size(); ++i) {
+    if (unique_nums.count(target - nums[i]) > 0) {
+      for (int j = 0; j < unique_nums[target - nums[i]].size(); ++j) {
+        if (unique_nums[target - nums[i]][j] != i) {
+          return vector<int>{i, unique_nums[target - nums[i]][j]};
+        }
+      }
+    }
+  }
+
+  return vector<int>();
+}
+
+int main() {
+  vector<int> test{4, 1, 2, 1};
+  vector<int> result = twoSum(test, 3);
+  for (int i = 0; i < result.size(); ++i) {
+    cout << result[i];
+  }
+  return 0;
+}
