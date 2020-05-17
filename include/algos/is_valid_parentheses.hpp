7442f4f0646b7691bdc29b2f9e10c9593ff8d922
Paul Pan <panjunhong05@gmail.com>
2020-05-17 16:56:40 -0700

    Added is valid parentheses question

---
+++ include/algos/is_valid_parentheses.hpp
@@ -0,0 +1,8 @@
+#ifndef ISVALIDPARENTHESES_HPP_
+#define ISVALIDPARENTHESES_HPP_
+
+#include <string>
+
+bool isValidParentheses(std::string s);
+
+#endif
---
+++ test/algos/is_valid_parentheses.cpp
@@ -0,0 +1,7 @@
+#include "algos/is_valid_parentheses.hpp"
+
+#include "testing/catch.hh"
+
+TEST_CASE("Testing parentheses validity", "[parentheses]") {
+  REQUIRE_FALSE(isValidParentheses("{}[]]"));
+}
