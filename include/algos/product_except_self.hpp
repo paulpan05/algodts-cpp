b67eaabbbce61364304621af79b1bed82271df3c
Paul Pan <panjunhong05@gmail.com>
2020-05-17 18:09:58 -0700

    Added product_except_self

---
+++ include/algos/product_except_self.hpp
@@ -0,0 +1,8 @@
+#ifndef PRODUCTEXCEPTSELF_HPP_
+#define PRODUCTEXCEPTSELF_HPP_
+
+#include <vector>
+
+std::vector<int> productExceptSelf(std::vector<int>& nums);
+
+#endif
---
+++ test/algos/product_except_self.cpp
@@ -0,0 +1,11 @@
+#include "algos/product_except_self.hpp"
+
+#include "testing/catch.hh"
+
+using namespace std;
+
+TEST_CASE("Testing product except self", "[product]") {
+  vector<int> test{1, 2, 3, 4};
+  vector<int> result{24, 12, 8, 6};
+  REQUIRE(productExceptSelf(test) == result);
+}
