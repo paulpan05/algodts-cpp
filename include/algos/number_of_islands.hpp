3200332ffe1327797f49f61efdfc9897c5c396c5
Paul Pan <panjunhong05@gmail.com>
2020-06-14 17:02:25 -0700

    Added num_islands algorithm and made CMakeList.txt generate compile_commands.json

---
+++ include/algos/number_of_islands.hpp
@@ -0,0 +1,8 @@
+#ifndef NUMBEROFISLANDS_HPP_
+#define NUMBEROFISLANDS_HPP_
+
+#include <vector>
+
+int numIslands(std::vector<std::vector<char>>& grid);
+
+#endif
---
+++ test/algos/number_of_islands.cpp
@@ -0,0 +1,10 @@
+#include "algos/number_of_islands.hpp"
+
+#include "testing/catch.hh"
+
+using namespace std;
+
+TEST_CASE("Checks number of islands in 2d array", "[num_islands]") {
+  vector<vector<char>> arr{{'1', '0', '0'}, {'1', '0', '0'}, {'1', '0', '0'}};
+  REQUIRE(numIslands(arr) == 1);
+}
